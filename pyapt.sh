49a292b473d2ab8a4ae70dedf00038ce7bf40ea9
jkutkut <jorgeregonzalez@gmail.com>
2021-10-19 07:44:39 +0200

    Bug on confirmation when installing fixed

--- pyapt.sh
+++ pyapt.sh
@@ -83,7 +83,7 @@ case $mode in
         fi
 
         # Confirm action
-        ask "The script is about to $mode the game $gameName. Do you want to continue?" "[yes]";
+        ask "The script is about to $mode the game $repoName. Do you want to continue?" "[yes]";
         if [ ! $askResponse = "yes" ]; then
             error "Aborted";
         fi
